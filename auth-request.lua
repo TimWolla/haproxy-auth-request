185a97745815f3af73ba7000b50be9698adbad45
Блажнов Василий (Blazhnov_VV) <Blazhnov_VV@directum.ru>
2019-09-05 15:32:53 +0500

    Passthrough location header only for 301 302 303 307 308 codes

--- auth-request.lua
+++ auth-request.lua
@@ -118,8 +118,8 @@ core.register_action("auth-request", { "http-req" }, function(txn, be, path)
 	if 200 <= c and c < 300 then
 		txn:set_var("txn.auth_response_successful", true)
 	-- Don't allow other codes.
-	-- 3xx: Passthrough location at redirect.
-	elseif 300 <= c and c < 400 then
+	-- Codes with Location: Passthrough location at redirect.
+	elseif c == 301 or c == 302 or c == 303 or c == 307 or c == 308 then
 		txn:set_var("txn.auth_response_location", h["location"])
 	-- 401 / 403: Do nothing, everything else: log.
 	elseif c ~= 401 and c ~= 403 then
